a12acbda434292b39f77f93d74aa44ad866bac7e
Szabolcs Gyuris <szimszon@oregpreshaz.eu>
2018-07-19 14:36:31 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# skill-yourextip
-OpsDroid skill to query for Internet facing IP address
+# opsdroid skill yourextip
+
+A skill for [opsdroid](https://github.com/opsdroid/opsdroid) to respond to what is your external ip.
+
+## Requirements
+
+* dnspython
+
+## Configuration
+
+* resolver_for_externa_ip -- you can set up the dns resolver to query for your external ip address, 
+    default: resolver1.opendns.com
+* domain_name_for_external_ip -- you can set the domain name to query for your external ip address,
+    default: myip.opendns.com
+
+## Usage
+
+#### `what is your external ip`
+
+Query for the external IP address.
+
+> user: what is your external ip
+>
+> opsdroid: My Internet facing IP address is x.y.z.w .
---
+++ __init__.py
@@ -0,0 +1,18 @@
+from opsdroid.matchers import match_regex
+import logging
+import dns.resolver
+
+def setup(opsdroid):
+    logging.debug("Loaded yourextip module")
+
+@match_regex(r'what is your (public|external|internet) ip|what is your external ip|what is your internet ip', case_sensitive=False)
+async def hello(opsdroid, config, message):
+    resolver_domain_for_query_external_ip = config.get("resolver_for_externa_ip","resolver1.opendns.com")
+    domain_name_to_query_for_external_ip = config.get("domain_name_for_external_ip","myip.opendns.com")
+    resolver=dns.resolver.Resolver()
+    resolver_for_external_ip = resolver.query(resolver_domain_for_query_external_ip,'a').rrset[0]
+    resolver.nameservers = [str(resolver_for_external_ip)]
+    my_external_ip = resolver.query(domain_name_to_query_for_external_ip).rrset[0]
+    text="My Internet facing IP address is " + str(my_external_ip) + " ."
+    await message.respond(text)
+
