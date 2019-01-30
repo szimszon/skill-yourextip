72fe0bbaf8c5e83d22bd9086e49083271f10624b
Szabolcs Gyuris <szimszon@oregpreshaz.eu>
2019-01-30 14:27:31 +0100

    config -> self.config

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ import dns.resolver
 class YourextipSkill(Skill):
   @match_regex(r'what is your (public|external|internet) ip', case_sensitive=False)
   async def yourextip(self, message):
-    resolver_domain_for_query_external_ip = config.get("resolver_for_externa_ip","resolver1.opendns.com")
-    domain_name_to_query_for_external_ip = config.get("domain_name_for_external_ip","myip.opendns.com")
+    resolver_domain_for_query_external_ip = self.config.get("resolver_for_externa_ip","resolver1.opendns.com")
+    domain_name_to_query_for_external_ip = self.config.get("domain_name_for_external_ip","myip.opendns.com")
     resolver=dns.resolver.Resolver()
     resolver_for_external_ip = resolver.query(resolver_domain_for_query_external_ip,'a').rrset[0]
     resolver.nameservers = [str(resolver_for_external_ip)]
